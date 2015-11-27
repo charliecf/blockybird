d776fd6527932eda1bbaf3e43c03a29f6d9b93c3
Charlie <charlie.feng@mail.mcgill.ca>
2015-11-27 09:41:10 -0500

    added jump function and restart game

--- main.js
+++ main.js
@@ -15,11 +15,32 @@ var mainState = {
 	// sets up the game after preload
 	create: function() {
 
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+
+		this.bird = this.game.add.sprite(100, 245, 'bird');
+
+		game.physics.arcade.enable(this.bird);
+		this.bird.body.gravity.y = 1000;
+
+		var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		spaceKey.onDown.add(this.jump, this);
+
 	},
 
 	// updates game 60 times per sec to make it work
 	update: function() {
 
+		if(this.bird.inWorld === false)
+			this.restartGame();
+
+	},
+
+	jump: function() {
+		this.bird.body.velocity.y = -350;
+	},
+
+	restartGame: function() {
+		game.state.start('main');
 	}
 
 };
