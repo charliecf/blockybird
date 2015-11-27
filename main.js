12aea9b8fb7cb8acbabbb3c85855d897a8daf9ae
Charlie <charlie.feng@mail.mcgill.ca>
2015-11-27 09:52:22 -0500

    added pipes and scoring

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var mainState = {
 
 		game.stage.backgroundColor = '#71c5cf';
 		game.load.image('bird', 'assets/bird.png');
+		game.load.image('pipe', 'assets/pipe.png');
 
 	},
 
@@ -17,21 +18,39 @@ var mainState = {
 
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 
+		// init the bird
 		this.bird = this.game.add.sprite(100, 245, 'bird');
 
+		// physics of the bird
 		game.physics.arcade.enable(this.bird);
 		this.bird.body.gravity.y = 1000;
 
+		// spacebar calls jump function()
 		var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 		spaceKey.onDown.add(this.jump, this);
+
+		// creating pipes
+		this.pipes = game.add.group();
+		this.pipes.enableBody = true;
+		this.pipes.createMultiple(20, 'pipe');
+
+		this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
+		// scoring
+		this.score = 0;
+		this.labelScore = game.add.text(20, 20, "0", {font: "30px Arial", fill: "#ffffff"});
 
 	},
 
 	// updates game 60 times per sec to make it work
 	update: function() {
 
-		if(this.bird.inWorld === false)
+		if(this.bird.inWorld === false) {
 			this.restartGame();
+		}
+
+		// if collision
+		game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
 
 	},
 
@@ -41,6 +60,32 @@ var mainState = {
 
 	restartGame: function() {
 		game.state.start('main');
+	},
+
+	addOnePipe: function(x, y) {
+		var pipe = this.pipes.getFirstDead();
+
+		pipe.reset(x, y);
+
+		pipe.body.velocity.x = -200;
+
+		// remove pipe once it's out
+		pipe.checkWorldBounds = true;
+		pipe.outOfBoundsKill = true;
+	},
+
+	addRowOfPipes: function() {
+		var hole = Math.floor(Math.random()*5) + 1;
+
+		for(var i = 0; i < 8; i++) {
+			if (i != hole && i !=hole + 1) {
+				this.addOnePipe(400, i * 60 + 10);
+			}
+		}
+
+		// add score when new pipe is created
+		this.score += 1;
+		this.labelScore.text = this.score;
 	}
 
 };
