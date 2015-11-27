ee2fce3c277c7b14d457de0f5a0759f429659554
Charlie <charlie.feng@mail.mcgill.ca>
2015-11-27 09:26:17 -0500

    set up main js functions and preload

--- main.js
+++ main.js
@@ -1,2 +1,28 @@
 // Initialize Phase, and create a 400x490px game
+var game = new Phaser.Game(400, 490, Phaser.AUTO, 'gameDiv');
 
+// Create the main stte which will contain the game
+var mainState = {
+
+	// sets up the assets of the game
+	preload: function() {
+
+		game.stage.backgroundColor = '#71c5cf';
+		game.load.image('bird', 'assets/bird.png');
+
+	},
+
+	// sets up the game after preload
+	create: function() {
+
+	},
+
+	// updates game 60 times per sec to make it work
+	update: function() {
+
+	}
+
+};
+
+game.state.add('main', mainState);
+game.state.start('main');
